3952bd737dffb7cfb1db7b14bd5c63db8556f863
Parth Choudhary <parth.dudhwal@gmail.com>
2023-08-16 12:28:11 +0530

    day 3 of LeetCode

---
+++ BinarySearch/Q410.java
@@ -0,0 +1,37 @@
+package BinarySearch;
+
+public class Q410 {
+    public static void main(String[] args) {
+        int[] nums = {7,2,5,10,8};
+        System.out.println(splitArray(nums, 2));
+    }
+
+    public static int splitArray(int[] nums, int k) {
+        int start = 0;
+        int end = 0;
+
+        for (int i=0;i<nums.length;i++){
+            start = Math.max(start,nums[i]);
+            end += nums[i];
+        }
+
+        while (start<end) {
+            int mid = start + (end - start) / 2;
+
+            int sum = 0;
+            int pieces = 1;
+
+            for (int num : nums) {
+                if (sum + num > mid) {
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+            if (pieces > k) start = mid + 1;
+            else end = mid;
+        }
+        return end;
+    }
+}
