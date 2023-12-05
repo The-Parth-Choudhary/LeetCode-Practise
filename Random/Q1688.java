5d5956848f9866d60f1d9cf23b27283c67b05878
Parth Choudhary <parth.dudhwal@gmail.com>
2023-12-05 16:47:35 +0530

    POD

--- Random/Q1688.java
+++ Random/Q1688.java
@@ -1,4 +1,24 @@
 package Random;
 
 public class Q1688 {
+    public static void main(String[] args) {
+        System.out.println(numberOfMatches(14));
+    }
+
+    public static int numberOfMatches(int n) {
+        int ans = 0;
+
+        while(n > 1){
+            ans += n / 2;
+
+            if(n % 2 == 0){
+                n = n / 2;
+            }
+            else{
+                n = n / 2 + 1;
+            }
+        }
+
+        return ans;
+    }
 }
