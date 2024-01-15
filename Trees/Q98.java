1d85e647fe6496e7859f7718497c698ed622312b
Parth Choudhary <parth.dudhwal@gmail.com>
2024-01-15 14:04:11 +0530

    valid BST

---
+++ Trees/Q98.java
@@ -0,0 +1,46 @@
+package Trees;
+
+public class Q98 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    long min = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+
+        if (root.val <= min) {
+            return false;
+        }
+
+        min = root.val;
+
+        if (!isValidBST(root.right)) {
+            return false;
+        }
+
+        return true;
+    }
+}
