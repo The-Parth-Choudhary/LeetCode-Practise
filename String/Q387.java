89d3db56cb6957cd6b3564637914e5981decc343
Parth Choudhary <parth.dudhwal@gmail.com>
2024-02-06 18:24:56 +0530

    random question

---
+++ String/Q387.java
@@ -0,0 +1,26 @@
+package String;
+
+import java.util.HashMap;
+
+public class Q387 {
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+        System.out.println(firstUniqChar(s));
+    }
+
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
